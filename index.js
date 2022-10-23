5e2de9bdbc49de4c208502ff309f48f661196537
Hala AlArid <halaalarid99@gmail.com>
2022-10-23 18:16:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const newCats = ["Milo", "Otis", "Garfield"];
+
+    newCats.push(name);
+
+    return newCats
+}
+
+function prependCat(name){
+    const newCats = ["Milo", "Otis", "Garfield"];
+
+    newCats.unshift(name);
+
+    return newCats
+}
+
+function removeLastCat(){
+    const newCats = ["Milo", "Otis", "Garfield"];
+
+    newCats.pop();
+
+    return newCats
+}
+
+function removeFirstCat(){
+    const newCats = ["Milo", "Otis", "Garfield"];
+
+    newCats.shift();
+
+    return newCats
+}
